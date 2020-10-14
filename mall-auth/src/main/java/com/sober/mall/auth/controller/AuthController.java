d6ec99d6ff8f7edf8637dec6ccdd23694219f867
gooodan <304028999@qq.com>
2020-10-14 20:40:18 +0800

    权限认证

---
+++ mall-auth/src/main/java/com/sober/mall/auth/controller/AuthController.java
@@ -0,0 +1,54 @@
+package com.sober.mall.auth.controller;
+
+import com.sober.common.api.CommonResult;
+import com.sober.common.constant.AuthConstant;
+import com.sober.mall.auth.domain.Oauth2TokenDto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.endpoint.TokenEndpoint;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
+
+import javax.annotation.Resource;
+import java.security.Principal;
+import java.util.Map;
+
+/**
+ * 自定义Oauth2获取令牌接口
+ * Created by sober on 2020/10/14.
+ */
+@RestController
+@Api(tags = "AuthController", description = "认证中心登录认证")
+@RequestMapping("/oauth")
+public class AuthController {
+
+    @Resource
+    private TokenEndpoint tokenEndpoint;
+
+    @ApiOperation("Oauth2获取token")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "grant_type", value = "授权模式", required = true),
+            @ApiImplicitParam(name = "client_id", value = "Oauth2客户端ID", required = true),
+            @ApiImplicitParam(name = "client_secret", value = "Oauth2客户端秘钥", required = true),
+            @ApiImplicitParam(name = "refresh_token", value = "刷新token"),
+            @ApiImplicitParam(name = "username", value = "登录用户名"),
+            @ApiImplicitParam(name = "password", value = "登录密码")
+    })
+    @PostMapping(value = "/token")
+    public CommonResult<Oauth2TokenDto> postAccessToken(@ApiIgnore Principal principal, @ApiIgnore @RequestParam Map<String, String> parameters) throws HttpRequestMethodNotSupportedException {
+        OAuth2AccessToken oAuth2AccessToken = tokenEndpoint.postAccessToken(principal, parameters).getBody();
+        Oauth2TokenDto oauth2TokenDto = Oauth2TokenDto.builder()
+                .token(oAuth2AccessToken.getValue())
+                .refreshToken(oAuth2AccessToken.getRefreshToken().getValue())
+                .expiresIn(oAuth2AccessToken.getExpiresIn())
+                .tokenHead(AuthConstant.JWT_TOKEN_PREFIX).build();
+
+        return CommonResult.success(oauth2TokenDto);
+    }
+
+
+}
